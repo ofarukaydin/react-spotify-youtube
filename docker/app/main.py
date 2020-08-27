9bfa6e0845aa4406cfa8ba7e6ee2f832cbff2766
Faruk Aydın <hayspat@gmail.com>
2020-08-28 01:47:19 +0300

    fix youtube scraping logic

--- docker/app/main.py
+++ docker/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import urllib.request
 import bs4
+import re
 from starlette.middleware.cors import CORSMiddleware
 
 
@@ -19,11 +20,21 @@ app.add_middleware(
 
 @app.get("/api/search")
 def read_item(q: str = None):
+    vid_urls = []
     textToSearch = q
-    query = urllib.parse.quote(textToSearch)
-    url = "https://www.youtube.com/results?search_query=" + query
+    search_term = urllib.parse.quote(textToSearch)
+    url = "https://www.youtube.com/results?search_query=" + search_term
     response = urllib.request.urlopen(url)
     html = response.read()
     soup = bs4.BeautifulSoup(html, 'html.parser')
+    print(soup)
     vid = soup.findAll(attrs={'class': 'yt-uix-tile-link'})
-    return {"url": 'https://www.youtube.com' + vid[0]['href']}
+
+    all_vid_id = re.findall(r'/watch\?v=(.{11})', str(soup))
+    if len(all_vid_id) >= 1:
+        for vid_id in all_vid_id:
+            vid_url = "https://www.youtube.com/watch?v=" + str(vid_id)
+            vid_urls.append(vid_url)
+        return {"url": vid_urls[0]}
+    else:
+        return 'Could not find any results with the query term: ' + search_term
